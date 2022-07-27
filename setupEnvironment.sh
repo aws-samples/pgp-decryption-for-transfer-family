d05882fbf82829ab6aebc52f70ef7cd4f6365e89
Lawton Pittenger <lawton.pittenger@gmail.com>
2022-07-27 16:30:36 -0400

    Update setupEnvironment.sh

--- setupEnvironment.sh
+++ setupEnvironment.sh
@@ -1,32 +1,12 @@
 #!/usr/bin/env bash
-echo Building required IAM policies...
 
-# Download IAM Policies from GitHub repo
-wget https://github.com/aws-samples/pgp-decryption-for-transfer-family/IAM_Policies.zip
-unzip IAM_Policies.zip
-
-# IAM Role Creation
-aws iam create-role --role-name PGPDecryptionLambdaExecutionRole --assume-role-policy-document file://lambda-trust-policy.json
-aws iam create-role --role-name PGPDecryptionManagedWorklowRole --assume-role-policy-document file://transfer-trust-policy.json
-
-# Attaching policies ot Lambda Execution Role
-aws iam put-role-policy --role-name PGPDecryptionLambdaExecutionRole --policy-name PGPDecryptionCloudWatchPolicy --policy-document file://CloudWatchPolicy.json
-aws iam put-role-policy --role-name PGPDecryptionLambdaExecutionRole --policy-name PGPDecryptionSecretsManagerPolicy --policy-document file://secretsManagerPolicy.json
-aws iam put-role-policy --role-name PGPDecryptionLambdaExecutionRole --policy-name PGPDecryptionS3Policy --policy-document file://s3Policy.json
-aws iam put-role-policy --role-name PGPDecryptionLambdaExecutionRole --policy-name PGPDecryptionTransferFamilyPolicy --policy-document file://transferFamilyPolicy.json
-
-# Attaching policies to Managed Workflow Execution Role
-aws iam put-role-policy --role-name PGPDecryptionManagedWorklowRole --policy-name PGPDecryptionManagedWorkflowPolicy --policy-document file://managedWorkflowPolicy.json
-aws iam put-role-policy --role-name PGPDecryptionManagedWorklowRole --policy-name PGPDecryptionTransferFamilyPolicy --policy-document file://transferFamilyPolicy.json
-
-# Installing required packages
+# Installing necessary python packages
 echo Installing required packages...
+cd /home/cloudshell-user
 sudo amazon-linux-extras enable python3.8
 sudo yum install python3.8 gcc make glibc-static bzip2 pip -y
 echo Required packages installed. 
 pip3.8 install virtualenv
-
-# Create and activate virtualenvironment to install required Python packages
 virtualenv python
 cd python
 . bin/activate
@@ -36,7 +16,7 @@ rm -rf ./bin
 mkdir ./python
 mv lib ./python/
 
-# Downloading GPG binary and compiling it from source
+# Downloading and building GPG binary from source
 wget https://www.gnupg.org/ftp/gcrypt/gnupg/gnupg-1.4.23.tar.bz2
 tar xjf gnupg-1.4.23.tar.bz2
 cd gnupg-1.4.23.tar.bz2
@@ -46,7 +26,23 @@ cp g10/gpg /home/cloudshell-user/python/python
 cd /home/cloudshell-user/python/python
 chmod o+x gpg
 cd ..
-
-# Zip everything up and push to Lambda as a layer
 zip -r lambdaLayer.zip python/
 aws lambda publish-layer-version --layer-name python-gnupg --description "Python-GNUPG Module and GPG Binary" --zip-file fileb://lambdaLayer.zip --compatible-runtimes python3.8
+cd ..
+
+# IAM Role Creation
+echo Creating IAM Roles...
+aws iam create-role --role-name PGPDecryptionLambdaExecutionRole --assume-role-policy-document file://./lambda-trust-policy.json
+aws iam create-role --role-name PGPDecryptionManagedWorklowRole --assume-role-policy-document file://./transfer-trust-policy.json
+
+# Attaching policies ot Lambda Execution Role
+aws iam put-role-policy --role-name PGPDecryptionLambdaExecutionRole --policy-name PGPDecryptionCloudWatchPolicy --policy-document file://./CloudWatchPolicy.json
+aws iam put-role-policy --role-name PGPDecryptionLambdaExecutionRole --policy-name PGPDecryptionSecretsManagerPolicy --policy-document file://./secretsManagerPolicy.json
+aws iam put-role-policy --role-name PGPDecryptionLambdaExecutionRole --policy-name PGPDecryptionS3Policy --policy-document file://./s3Policy.json
+aws iam put-role-policy --role-name PGPDecryptionLambdaExecutionRole --policy-name PGPDecryptionTransferFamilyPolicy --policy-document file://./transferFamilyPolicy.json
+
+# Attaching policies to Managed Workflow Execution Role
+aws iam put-role-policy --role-name PGPDecryptionManagedWorklowRole --policy-name PGPDecryptionManagedWorkflowPolicy --policy-document file://./managedWorkflowPolicy.json
+aws iam put-role-policy --role-name PGPDecryptionManagedWorklowRole --policy-name PGPDecryptionTransferFamilyPolicy --policy-document file://./transferFamilyPolicy.json
+
+echo Success: IAM roles created
